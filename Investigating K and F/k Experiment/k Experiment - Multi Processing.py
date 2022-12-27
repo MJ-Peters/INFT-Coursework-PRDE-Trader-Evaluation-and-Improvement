8b67bd9a68e7dbf49ff4e61f341144bed96bc840
Marshall Peters <mj_peters@btconnect.com>
2022-12-27 14:53:10 +0000

    Adjusted n_runs_... to allow for further multiprocessing.

--- Investigating K and F/k Experiment/k Experiment - Multi Processing.py
+++ Investigating K and F/k Experiment/k Experiment - Multi Processing.py
@@ -17,7 +17,13 @@ startTime = time.time()
 
 
 # Small alteration made to save each market session plot as its own png
-def n_runs_plot_trades(n, trial_id, start_time, end_time, traders_spec, order_sched, k_value, x):
+def n_runs_plot_trades(n, trial_id, start_time, end_time, order_sched, k_value, x):
+
+    # Introducing the traders to the market
+    trader_params = {"k": k_value, "F": 0.8, "s_min": -1.0, "s_max": +1.0, "wait_time": wait_time}
+    sellers_spec = [("PRDE", 30, trader_params)]
+    buyers_spec = sellers_spec
+    traders_spec = {"sellers": sellers_spec, "buyers": buyers_spec}
 
     for i in range(n):  # Run 0 has already been completed so will start at run 1
         if x == 1:  # runs 1, 2, 3
@@ -52,79 +58,79 @@ def n_runs_plot_trades(n, trial_id, start_time, end_time, traders_spec, order_sc
             plt.show()
 
 
-def get_order_price(i, sched, n, mode):
-    pmin = min(sched[0][0], sched[0][1])
-    pmax = max(sched[0][0], sched[0][1])
-    prange = pmax - pmin
-    stepsize = prange / (n - 1)
-    halfstep = round(stepsize / 2.0)
-
-    if mode == 'fixed':
-        orderprice = pmin + int(i * stepsize)
-    elif mode == 'jittered':
-        orderprice = pmin + int(i * stepsize) + random.randint(-halfstep, halfstep)
-    elif mode == 'random':
-        if len(sched) > 1:
-            # more than one schedule: choose one equiprobably
-            s = random.randint(0, len(sched) - 1)
-            pmin = min(sched[s][0], sched[s][1])
-            pmax = max(sched[s][0], sched[s][1])
-        orderprice = random.randint(pmin, pmax)
-    return orderprice
-
-
-# Small alteration made to save plot as a png to computer
-def make_supply_demand_plot(bids, asks):
-    # total volume up to current order
-    volS = 0
-    volB = 0
-
-    fig, ax = plt.subplots()
-    plt.ylabel('Price')
-    plt.xlabel('Quantity')
-
-    pr = 0
-    for b in bids:
-        if pr != 0:
-            # vertical line
-            ax.plot([volB, volB], [pr, b], 'r-')
-        # horizontal lines
-        line, = ax.plot([volB, volB + 1], [b, b], 'r-')
-        volB += 1
-        pr = b
-    if bids:
-        line.set_label('Demand')
-
-    pr = 0
-    for s in asks:
-        if pr != 0:
-            # vertical line
-            ax.plot([volS, volS], [pr, s], 'b-')
-        # horizontal lines
-        line, = ax.plot([volS, volS + 1], [s, s], 'b-')
-        volS += 1
-        pr = s
-    if asks:
-        line.set_label('Supply')
-
-    if bids or asks:
-        plt.legend()
-    plt.savefig("sup_dem.png")
-    plt.show()
-
-
-def plot_sup_dem(seller_num, sup_ranges, buyer_num, dem_ranges, stepmode):
-    asks = []
-    for s in range(seller_num):
-        asks.append(get_order_price(s, sup_ranges, seller_num, stepmode))
-    asks.sort()
-    bids = []
-    for b in range(buyer_num):
-        bids.append(get_order_price(b, dem_ranges, buyer_num, stepmode))
-    bids.sort()
-    bids.reverse()
-
-    make_supply_demand_plot(bids, asks)
+# def get_order_price(i, sched, n, mode):
+#     pmin = min(sched[0][0], sched[0][1])
+#     pmax = max(sched[0][0], sched[0][1])
+#     prange = pmax - pmin
+#     stepsize = prange / (n - 1)
+#     halfstep = round(stepsize / 2.0)
+#
+#     if mode == 'fixed':
+#         orderprice = pmin + int(i * stepsize)
+#     elif mode == 'jittered':
+#         orderprice = pmin + int(i * stepsize) + random.randint(-halfstep, halfstep)
+#     elif mode == 'random':
+#         if len(sched) > 1:
+#             # more than one schedule: choose one equiprobably
+#             s = random.randint(0, len(sched) - 1)
+#             pmin = min(sched[s][0], sched[s][1])
+#             pmax = max(sched[s][0], sched[s][1])
+#         orderprice = random.randint(pmin, pmax)
+#     return orderprice
+#
+#
+# # Small alteration made to save plot as a png to computer
+# def make_supply_demand_plot(bids, asks):
+#     # total volume up to current order
+#     volS = 0
+#     volB = 0
+#
+#     fig, ax = plt.subplots()
+#     plt.ylabel('Price')
+#     plt.xlabel('Quantity')
+#
+#     pr = 0
+#     for b in bids:
+#         if pr != 0:
+#             # vertical line
+#             ax.plot([volB, volB], [pr, b], 'r-')
+#         # horizontal lines
+#         line, = ax.plot([volB, volB + 1], [b, b], 'r-')
+#         volB += 1
+#         pr = b
+#     if bids:
+#         line.set_label('Demand')
+#
+#     pr = 0
+#     for s in asks:
+#         if pr != 0:
+#             # vertical line
+#             ax.plot([volS, volS], [pr, s], 'b-')
+#         # horizontal lines
+#         line, = ax.plot([volS, volS + 1], [s, s], 'b-')
+#         volS += 1
+#         pr = s
+#     if asks:
+#         line.set_label('Supply')
+#
+#     if bids or asks:
+#         plt.legend()
+#     plt.savefig("sup_dem.png")
+#     plt.show()
+#
+#
+# def plot_sup_dem(seller_num, sup_ranges, buyer_num, dem_ranges, stepmode):
+#     asks = []
+#     for s in range(seller_num):
+#         asks.append(get_order_price(s, sup_ranges, seller_num, stepmode))
+#     asks.sort()
+#     bids = []
+#     for b in range(buyer_num):
+#         bids.append(get_order_price(b, dem_ranges, buyer_num, stepmode))
+#     bids.sort()
+#     bids.reverse()
+#
+#     make_supply_demand_plot(bids, asks)
 
 
 """End of functions from week 8 BSE workshop"""
@@ -142,15 +148,6 @@ supply_schedule = [{'from': start_time, 'to': end_time, 'ranges':
                     [sup_range], 'stepmode': stepmode}]
 demand_schedule = [{'from': start_time, 'to': end_time, 'ranges':
                     [dem_range], 'stepmode': stepmode}]
-
-k_vals = [5, 6, 7]
-# Introducing the traders to the market
-trader_params = {"k": k, "F": 0.8, "s_min": -1.0, "s_max": +1.0, "wait_time": wait_time}
-sellers_spec = [("PRDE", 30, trader_params)]
-seller_num = 30
-buyers_spec = sellers_spec
-buyer_num = seller_num
-traders_spec = {"sellers": sellers_spec, "buyers": buyers_spec}
 
 # Defining the order schedule, interval 10 -> ~2.6e5 orders given to each trader over the 30 days
 # drip-poisson time mode is used to more closely simulate a real market
@@ -176,42 +173,60 @@ y = np.empty(0)
 # Runs the interval n times from start to finish to plot results and supply/demand chart
 # plot_sup_dem(seller_num, [sup_range], buyer_num, [dem_range], stepmode)  # Same supply/demand as baseline
 
+# Split 9 runs of k=5 into 3x3 runs of k=6 to run in parallel
 k1_1 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 5, 1))
+                             args=(n, trial_id, start_time, end_time, order_sched, 5, 1))
 
 k1_2 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 5, 2))
+                             args=(n, trial_id, start_time, end_time, order_sched, 5, 2))
 
 k1_3 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 5, 3))
+                             args=(n, trial_id, start_time, end_time, order_sched, 5, 3))
 
-
+# Split 9 runs of k=6 into 3x3 runs of k=6 to run in parallel
 k2_1 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 6, 1))
+                             args=(n, trial_id, start_time, end_time, order_sched, 6, 1))
 
 k2_2 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 6, 2))
+                             args=(n, trial_id, start_time, end_time, order_sched, 6, 2))
 
 k2_3 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 6, 3))
+                             args=(n, trial_id, start_time, end_time, order_sched, 6, 3))
 
-
+# Split 9 runs of k=7 into 3x3 runs of k=6 to run in parallel
 k3_1 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 7, 1))
+                             args=(n, trial_id, start_time, end_time, order_sched, 7, 1))
 
 k3_2 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 7, 2))
+                             args=(n, trial_id, start_time, end_time, order_sched, 7, 2))
 
 k3_3 = multiprocessing.Process(target=n_runs_plot_trades,
-                             args=(n, trial_id, start_time, end_time, traders_spec, order_sched, 7, 3))
+                             args=(n, trial_id, start_time, end_time, order_sched, 7, 3))
 
+# Uni virtual machines have 16 virtual processors so can handle 9 parallel processes
 if __name__ == "__main__":
-    k1.start()
-    k2.start()
-    k3.start()
-    k1.join()
-    k2.join()
-    k3.join()
+    # Simultaneously starts all processes
+    k1_1.start()
+    k1_2.start()
+    k1_3.start()
+    k2_1.start()
+    k2_2.start()
+    k2_3.start()
+    k3_1.start()
+    k3_2.start()
+    k3_3.start()
+    # Waits until the thread that was started to complete before going to the next bit of code
+    k1_1.join()
+    k1_2.join()
+    k1_3.join()
+    k2_1.join()
+    k2_2.join()
+    k2_3.join()
+    k3_1.join()
+    k3_2.join()
+    k3_3.join()
+
+
 
 executionTime = (time.time() - startTime)
 print("Execution time in seconds: " + str(executionTime))
